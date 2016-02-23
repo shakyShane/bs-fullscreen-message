281433018e5a0f988678d5cb3256754bc82c1a3d
Thunf <458859360@qq.com>
2016-02-23 11:05:57 +0800

    fix elems remove BUG & update exam

--- client.js
+++ client.js
@@ -20,20 +20,18 @@
         overflow: 'scroll'
     };
 
-    var elem = document.createElement('div');
-    var body = document.getElementsByTagName('body')[0];
-    var int;
-    var key;
-
-    for (key in styles) {
-        elem.style[key] = styles[key];
-    }
+    var body = document.getElementsByTagName('body')[0],
+        elems = [], 
+        elem, 
+        int;
 
     socket.on(CLEAR_EVENT, function () {
         if (int) {
             clearTimeout(int);
         }
-        body.removeChild(elem);
+        for (var e in elems) {
+            body.removeChild(e);
+        }
     });
     socket.on(MSG_EVENT, function (data) {
 
@@ -46,24 +44,32 @@
             elem = document.createElement('script');
             html = data.script;
         }else{
+            elem = document.createElement('div');
+            for (var key in styles) {
+                elem.style[key] = styles[key];
+            }
             html = '<h1 style="%s">%s</h1><div style="%s"><pre style="%s">%s</pre></div>'
-            .replace('%s', data.titleStyles   || 'font-family:sans-serif')
-            .replace('%s', data.title         || 'Message from Browsersync')
-            .replace('%s', data.wrapperStyles || 'padding: 20px;border: 2px dashed #393D49; overflow:auto; color: #BEBEBE')
-            .replace('%s', data.preStyles     || 'white-space:pre')
-            .replace('%s', data.body          || 'No msg provided, please check the console')
+                .replace('%s', data.titleStyles   || 'font-family:sans-serif')
+                .replace('%s', data.title         || 'Message from Browsersync')
+                .replace('%s', data.wrapperStyles || 'padding: 20px;border: 2px dashed #393D49; overflow:auto; color: #BEBEBE')
+                .replace('%s', data.preStyles     || 'white-space:pre')
+                .replace('%s', data.body          || 'No msg provided, please check the console')
         }
 
         elem.innerHTML = html;
-
         body.appendChild(elem);
 
-        int = setTimeout(function () {
-            if (elem.parentNode && 0 != data.timeout) {
-                body.removeChild(elem);
-            }
-            clearTimeout(int);
-            int = undefined;
-        }, data.timeout || TIMEOUT);
+        elems.push(elem);
+
+        (function (elem, int, data) {
+            int = setTimeout(function () {
+                if (elem.parentNode && 0 != data.timeout) {
+                    body.removeChild(elem);
+                }
+                clearTimeout(int);
+                int = undefined;
+            }, data.timeout || TIMEOUT);
+        })(elem, int, data);
+
     });
 })(window.___browserSync___.socket);
--- example.js
+++ example.js
@@ -13,19 +13,30 @@ setTimeout(function () {
 }, 5000);
 
 setTimeout(function () {
+    var exam = ["value here", {such: 1234}];
     browserSync.sockets.emit('fullscreen:message', {
-        timeout: 0,
-        script: script(function(){
+        timeout: 100,
+        script: script(function(data){
             var body = document.getElementsByTagName('body')[0];
             var elem = document.createElement('script');
             body.appendChild(elem);
-            elem.innerHTML = "alert('other message or act')";
+            elem.innerHTML = "alert('See " + data.exam[0] + data.exam[1]['such'] + "')";
+        }, {
+            exam: exam
         })
     });
-}, 10000);
+}, 7000);
 
-// get script by fn
-function script(fn){
+// get script by fn, get data by vals
+function script(fn, vals){
     if ('function' != typeof fn) {return ''};
-    return '('+ fn.toString() +')();';
+    var data = {};
+    if ('object' == typeof vals) {
+        for (var k in vals) {
+            if ('undefined' != typeof vals[k]) {
+                data[k] = vals[k];
+            };
+        }
+    };
+    return '(' + fn.toString() + ')(' + JSON.stringify(data) + ');';
 }
